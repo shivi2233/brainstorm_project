6fae44b3328e73d060df35b25376634e5d662829
shivi2233 <shivi.universal@gmail.com>
2025-11-10 23:15:08 +0530

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+def get_majority(votes):
+    """Return majority option among players"""
+    if not votes:
+        return None
+    count = Counter(votes)
+    return count.most_common(1)[0][0]
