8dcbd730874a07b0d7160d45e0511fa4bee69770
shivi2233 <shivi.universal@gmail.com>
2025-11-13 23:27:07 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,8 +1,9 @@
 from collections import Counter
-
+#vkmckyhmckyh
 def get_majority(votes):
     """Return majority option among players"""
     if not votes:
         return None
     count = Counter(votes)
     return count.most_common(1)[0][0]
+
